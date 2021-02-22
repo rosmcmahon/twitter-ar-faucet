5ad6ae4a8ba99b649b0bdb2adf44628ca022324f
Ros McMahon <rosmcmahon@gmail.com>
2021-02-22 18:43:21 +0000

    set up metrics http server

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import greenlock from 'greenlock-express'
 import { parse } from 'url'
 import next from 'next'
-import { IncomingMessage, ServerResponse } from 'http'
+import http, { IncomingMessage, ServerResponse } from 'http'
 import { logger } from './utils/logger'
+import { register } from 'prom-client'
 
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
@@ -19,14 +20,12 @@ greenlock
 
 const mainHandler = async (req: IncomingMessage, res: ServerResponse) => {
 	const parsedUrl = parse(req.url!, true)
-	const { pathname, /*query*/ } = parsedUrl
+	const { pathname } = parsedUrl
 
-	// if(dev){ // need some kind of authentication for production
-	// 	if(pathname === '/metrics'){
-	// 		res.writeHead(200, { 'Content-Type': 'text/plain'})
-	// 		res.end(await register.metrics())
-	// 	}
-	// }
+	if(pathname === '/metrics'){
+		res.writeHead(200, { 'Content-Type': 'text/plain'})
+		res.end(await register.metrics())
+	}
 	
 	nextHandler(req, res) //, parsedUrl)
 }
@@ -36,26 +35,35 @@ function httpsWorker(glx: greenlock.glx) {
 
 		logger('* SERVER WAS STARTED *')
 
+		http.createServer(async(req: IncomingMessage, res: ServerResponse) => {
+			const parsedUrl = parse(req.url!, true)
+			const { pathname } = parsedUrl
+		
+			if(pathname === '/metrics'){
+				res.writeHead(200, { 'Content-Type': 'text/plain'})
+				res.end(await register.metrics())
+			}
+		}).listen(9100, "0.0.0.0", ()=> console.info('metrics on http://localhost:9100/metrics'))
+
 		if(dev){
 			let httpServer = glx.httpServer(mainHandler)
 
-			httpServer.listen(3210, "0.0.0.0", function() {
+			httpServer.listen(3210, "0.0.0.0", ()=> {
 				console.info("Dev mode. Listening on ", httpServer.address(), 'http://localhost:3210')
-			});
+			})
 		} else {
 
 			// Note: You must ALSO listen on port 80 for ACME HTTP-01 Challenges
 			// (the ACME and http->https middleware are loaded by glx.httpServer)
-			let httpServer = glx.httpServer();
-			httpServer.listen(80, "0.0.0.0", function() {
+			const httpServer = glx.httpServer()
+			httpServer.listen(80, "0.0.0.0", ()=> {
 				console.info("Listening on ", httpServer.address(), " but redirecting to https")
-			});
-			// Get the raw https server:
-			let httpsServer = glx.httpsServer(null, mainHandler)
+			})
 
+			const httpsServer = glx.httpsServer(null, mainHandler)
 			httpsServer.listen(443, "0.0.0.0", function() {
 				console.info("Listening on ", httpsServer.address())
-			});
+			})
 		}
 	})
 }
