6add8cd9b68b97abca74bd6000f4a69f719b2cc1
Ros McMahon <rosmcmahon@gmail.com>
2021-03-11 15:18:30 +0000

    bug! logToSlack without try/catch

--- utils/slack-logger.ts
+++ utils/slack-logger.ts
@@ -1,40 +1,47 @@
 import { IncomingWebhook } from '@slack/webhook'
 import { EOL } from 'os'
 import { BotCheckResult } from '../services/bot-check'
+import { logger } from './logger'
 
 const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK!)
 
 export const logToSlack = async (handle: string, twitterId: string, address: string, botresult: BotCheckResult, tweetId?: string) => {
 
-	let text = ''
-		+ `DisplayName: <https://twitter.com/${handle}|${handle}>` + EOL
-		+ `TwitterID: \`${twitterId}\`` + EOL
-		+ `Claim Address: <https://viewblock.io/arweave/address/${address}|${address}>` + EOL
-		+ `Bot-Score: \`${botresult.botScore}\`` + EOL
-		+ `Successful: \`${botresult.passed}\`` + EOL
-		+ `Reason: \`${botresult.reason}\`` + EOL
-	if(tweetId){
-		text += 'Tweet Reply: ' + `<https://twitter.com/ArweaveW/status/${tweetId}|LINK>` + EOL
-	}
+	if(process.env.NODE_ENV !== 'production') return
 
-	const res = await webhook.send({
-		"username": 'Twitter Cannon 🤖',
-		"blocks": [
-			{
-				"type": "section", 
-				"text": {
-					"type": "mrkdwn",
-					"text": `*${new Date().toUTCString()} - <https://twitter.com/${handle}|${handle}>*`,
-				} 
-			},
-			{
-				"type": "section", 
-				"text": {
-					"type": "mrkdwn",
-					"text": text,
-				} 
-			}
-		]
-	})
-	return res
+	try{
+		let text = ''
+			+ `DisplayName: <https://twitter.com/${handle}|${handle}>` + EOL
+			+ `TwitterID: \`${twitterId}\`` + EOL
+			+ `Claim Address: <https://viewblock.io/arweave/address/${address}|${address}>` + EOL
+			+ `Bot-Score: \`${botresult.botScore}\`` + EOL
+			+ `Successful: \`${botresult.passed}\`` + EOL
+			+ `Reason: \`${botresult.reason}\`` + EOL
+		if(tweetId){
+			text += 'Tweet Reply: ' + `<https://twitter.com/ArweaveW/status/${tweetId}|LINK>` + EOL
+		}
+
+		const res = await webhook.send({
+			"username": 'Twitter Cannon 🤖',
+			"blocks": [
+				{
+					"type": "section", 
+					"text": {
+						"type": "mrkdwn",
+						"text": `*${new Date().toUTCString()} - <https://twitter.com/${handle}|${handle}>*`,
+					} 
+				},
+				{
+					"type": "section", 
+					"text": {
+						"type": "mrkdwn",
+						"text": text,
+					} 
+				}
+			]
+		})
+		return res
+	}catch(e){
+		logger('error in logToSlack', e.code, ':', e.message)
+	}
 }
