8ef6163f77da0d455a2698829df21c22176a29cd
Ros McMahon <rosmcmahon@gmail.com>
2021-07-13 10:57:45 +0700

    wrap arweave.net calls in try/catch

--- services/ar-transfer.ts
+++ services/ar-transfer.ts
@@ -7,19 +7,19 @@ import { slackLogger } from '../utils/slack-logger'
 const arweave = Arweave.init({ host: 'arweave.net', timeout: 60000 })
 
 export const transferAr = async (address: string) => {
-	const jwk = require('../secrets/jwk.json')
-	let fee = await arweave.transactions.getPrice(0, address)
-	logger(address, 'WALLET_GEN_FEE', fee,'winston', arweave.ar.winstonToAr(fee), 'AR')
-	let tx = await arweave.createTransaction({
-		target: address,
-		quantity: arweave.ar.arToWinston(REWARD_AR),
-	}, jwk)
-	tx.addTag('App-Name', 'twitter-cannon')
-
-	/* TODO: implement a whole retry post tx scenario */
-	/* We did the above and created a library for it as it became such an issue */
-
 	try {
+		const jwk = require('../secrets/jwk.json')
+		let fee = await arweave.transactions.getPrice(0, address)
+		logger(address, 'WALLET_GEN_FEE', fee,'winston', arweave.ar.winstonToAr(fee), 'AR')
+		let tx = await arweave.createTransaction({
+			target: address,
+			quantity: arweave.ar.arToWinston(REWARD_AR),
+		}, jwk)
+		tx.addTag('App-Name', 'twitter-cannon')
+
+		/* TODO: implement a whole retry post tx scenario */
+		/* We did the above and created a library for it as it became such an issue */
+
 		const txid = await uploadTx(tx, jwk, address)
 		logger(address, 'AR transfer success. txid:', txid)
 	} catch (e) {
