f525293537aa3bcf7ea8ea1528c917d1bd04fc26
Ros McMahon <rosmcmahon@gmail.com>
2023-02-15 22:32:56 +0700

    fix old arweave.org footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,35 +10,32 @@ const Footer = () => {
 						<div className="footer__section space">
 							<a href="https://www.arweave.org/"><img src="/assets/footer-logo.svg"/></a>
 							<div className="links">
-								<a href="//docs.arweave.org/info/wallets/arweave-web-extension-wallet">Arweave Wallet</a>
-								<a href="https://www.arweave.org/technology#papers">Papers</a>
-								<a href="https://www.arweave.org/technology#blockweaves">Blockweaves and Proof of Access</a>
-								<a href="https://www.arweave.org/technology#endowment">Storage Endowment</a>
-								<a href="https://www.arweave.org/technology#permaweb">Permaweb</a>
-								<a href="https://www.arweave.org/technology#content-moderation">Content Moderation</a>
-								<a href="https://www.arweave.org/files/arweave-press-kit-2020.zip">Press Kit</a>
-								<a href="//www.arweave.org/cookies-policy">Privacy Policy</a>
+								<a href="//docs.arweave.org/info/wallets">Arweave Wallet</a>
+								<a href="//yellow-paper.arweave.dev">Yellow Paper</a>
+								<a href="//2-6-spec.arweave.dev">2.6 Spec</a>
+								<a href="//docs.arweave.org/policies/privacy-policy">Privacy Policy</a>
 							</div>
 						</div>
 						<div className="footer__section space">
 							<h4>Build</h4>
-							<div className="links space_more"><a href="https://www.arweave.org/build">Start</a>
-								<a href="https://www.arweave.org/build#interfaces">Interfaces</a>
-								<a href="https://www.arweave.org/build#resources">Resources</a>
+							<div className="links space_more">
+								<a href="https://www.arweave.org/build">Start</a>
+								<a href="https://github.com/ArweaveTeam/arweave-js">ArweaveJS</a>
+								<a href="https://cookbook.arweave.dev/getting-started/welcome.html">Cookbook</a>
 							</div>
 						</div>
 						<div className="footer__section space">
 							<h4>Mine</h4>
-							<div className="links space_more"><a href="https://www.arweave.org/mine/start">Start</a>
-								<a href="https://www.arweave.org/mine/optimise">Optimise</a>
-								<a href="https://www.arweave.org/mine/learn-more">Learn more</a>
+							<div className="links space_more"><a href="//docs.arweave.org/mine/start">Start</a>
+								<a href="//docs.arweave.org/mine/optimise">Optimise</a>
+								<a href="//docs.arweave.org/mine/learn-more">Learn more</a>
 							</div>
 						</div>
 						<div className="footer__section space">
 							<h4>Get involved</h4>
 							<div className="links space_more">
-								<a href="https://www.arweave.org/get-involved/community">Community</a>
-								<a href="https://www.arweave.org/get-involved/grants-funding">Grants &amp; Funding</a>
+								<a href="https://discord.gg/AhsZfBm">Community</a>
+								<a href="https://www.arweave.org/funding">Grants &amp; Funding</a>
 								<a href="http://arweave.jobs">Ecosystem Jobs</a>
 							</div>
 						</div>
